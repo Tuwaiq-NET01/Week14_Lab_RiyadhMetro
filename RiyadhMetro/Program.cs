97651d5f7c75899353a778b73c63619a5cf43ec3
Mohammed Rashed <Mohammed.Albalawi@tuwaiq.edu.sa>
2021-08-02 13:51:02 +0300

    list

---
+++ RiyadhMetro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RiyadhMetro
+{
+    class Program
+    {
+        public static List<string> red = new List<string>() { "Alwrod", "Salah Aldin", "Alnzha", "Hittin", "Qwrtba", "KSU", "Almalaz", "Diplomatic" };
+        public static List<string> blue = new List<string>() { "Alakeek", "Alyasmin", "Salah Aldin", "Rahmanea", "Motamrat", "Alolya", "Almoroj" };
+        public static List<string> green = new List<string>() { "Nmar", "PNU", "Salah Aldin", "Solaymanea", "Alamal", "Tayba", "Alhazem", "Alzahra" };
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
